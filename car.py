26019e31fd4322f135a0f18950c89554e541b782
alex <a.petrenko@sam-solutions.biz>
2018-01-26 13:13:09 +0200

    first commit

---
+++ car.py
@@ -0,0 +1,48 @@
+import gpio as GPIO
+
+class Car:
+
+    servoPin = 11
+    IN3 = 5
+    IN4 = 4
+    ENB = 3
+
+    def __init__(self):
+        ''' initialization of pins '''
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(self.servoPin, GPIO.OUT)
+        GPIO.setup(self.ENB, GPIO.OUT)
+        GPIO.setup(self.IN3, GPIO.OUT)
+        GPIO.setup(self.IN4, GPIO.OUT)
+        # 50 and 40 are frequency
+        self.servo = GPIO(self.servoPin, 50)
+        self.motor = GPIO(self.ENB, 40)
+
+
+    def straightDirect(self):
+        '''make staight direct'''
+        GPIO.output(self.IN3, 1)
+        GPIO.output(self.IN4, 0)
+
+    def reverseDirect(self):
+        ''' make reverse direct '''
+        GPIO.output(self.IN3, 0)
+        GPIO.output(self.IN4, 1)
+
+
+    def changeAngle(self, angle):
+        ''' turning '''
+        DC = 1./18.*(angle)+2
+        self.servo.ChangeDutyCycle(DC)
+
+    def speed(self, speed):
+        ''' control speed '''
+        self.motor.ChangeDutyCycle(speed)
+
+    def stop(self):
+        ''' stop the car '''
+        self.motor.ChangeDutyCycle(0)
+
+    def activeStop(self):
+        ''' active stop '''
+        self.reverseDirect()
---
+++ functions.py
@@ -0,0 +1,94 @@
+
+def eachPixelOfLine(line,centerY):
+    xStart = line[2] if line[0] > line[2] else line[0]
+    xEnd = line[2] if line[0] < line[2] else line[0]
+    yStart = line[3] if line[1] > line[3] else line[1]
+    yEnd = line[3] if line[1] < line[3] else line[1]
+
+    reverse = True if line[1] > line[3] else False
+
+
+    xLen = xEnd - xStart
+    yLen = yEnd - yStart
+
+
+    if (xLen > yLen):
+        xPixels = range(xStart, xEnd)
+        step = yLen / xLen
+        step = round(step, 2)
+
+        yAcc = yStart
+
+        for x in xPixels[1:]:
+            yAcc = yAcc + step
+            #if (int(round(yAcc)) == centerY):
+            #yield [x, int(round(yAcc))]
+
+    else:
+        yPixels = range(yStart,yEnd)
+        step = xLen / yLen
+        step = round(step, 2)
+
+        xAcc = xStart
+        if (reverse):
+            for y in reversed(yPixels[1:]):
+                xAcc = xAcc + step
+                if (y == centerY):
+                    yield [int(round(xAcc)), y]
+        else:
+            for y in yPixels[1:]:
+                xAcc = xAcc + step
+                if (y == centerY):
+                    yield [int(round(xAcc)), y]
+
+
+
+
+def getTheNearestLine(lines, centerX, centerY, img):
+
+    nearestRigthX = 688
+    nearestLeftX = 0
+    for line in lines:
+
+        for linePixel in eachPixelOfLine(line, centerY):
+
+            x = linePixel[0]
+            y = linePixel[1]
+
+            if (y != centerY):
+                continue
+            if (x > centerX):
+                if (centerX - x < nearestRigthX):
+                    nearestRigthX = x
+            else:
+                if (centerX - x > nearestLeftX):
+                    nearestLeftX = x
+
+    return [nearestLeftX, nearestRigthX, centerY]
+
+# kalman settings
+varVolt = 49
+varProcess = 0.5
+Pc = 0.0
+G = 0.0
+P = 1.0
+Xp = 0.0
+Zp = 0.0
+Xe = 0.0
+
+def kalman(val):
+    global P
+    global varProcess
+    global Xe
+    global Xp
+    global Zp
+    global G
+    global Pc
+    global varVolt
+    Pc = P + varProcess
+    G = Pc/(Pc + varVolt)
+    P = (1 - G)*Pc
+    Xp = Xe
+    Zp = Xp
+    Xe = G*(val-Zp)+Xp
+    return int(Xe)
