1177d52ad79d905f2383763e1b15c592d2087725
Dan Kurasov <dionisiy13@gmail.com>
2018-04-18 13:27:30 +0200

    Fixed

--- main.py
+++ main.py
@@ -109,21 +109,7 @@ def main():
         masked_image = draw_binary_mask(binary_mask, hsv_image)
 
 
-        low_threshold = 50
-        high_threshold = 150
-        edges = cv2.Canny(masked_image, low_threshold, high_threshold)
-
-
-        rho = 1  # distance resolution in pixels of the Hough grid
-        theta = np.pi / 90  # angular resolution in radians of the Hough grid
-        threshold = 15  # minimum number of votes (intersections in Hough grid cell)
-        min_line_length = 20  # minimum number of pixels making up a line
-        max_line_gap = 15  # maximum gap in pixels between connectable line segments
-        line_image = np.copy(newImage) * 0  # creating a blank to draw lines on
-        lines = []
-        # Run Hough on edge detected image
-        # Output "lines" is an array containing endpoints of detected line segments
-        
+      
 
 
 
