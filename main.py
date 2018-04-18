971efd60b68a881873aaca9958fe69f29eca3579
Dan Kurasov <dionisiy13@gmail.com>
2018-04-18 13:55:50 +0200

    Fixed

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import time
 from functions import *
 from imutils.video.pivideostream import PiVideoStream
 import imutils
-import random
 from pprint import pprint
 from picamera.array import PiRGBArray
 from picamera import PiCamera
@@ -80,14 +79,6 @@ def main():
 
         imgOriginal = camera.read()
         imgOriginal = imutils.resize(imgOriginal, 500, 300)
-        
-        # make the frame more bright
-        lab = cv2.cvtColor(imgOriginal, cv2.COLOR_BGR2LAB)
-        l, a, b = cv2.split(lab)
-        clahe = cv2.createCLAHE(clipLimit=4.0, tileGridSize=(8,8))
-        cl = clahe.apply(l)
-        limg = cv2.merge((cl,a,b))
-        #imgOriginal = cv2.cvtColor(limg, cv2.COLOR_LAB2BGR)
 
 
         image = imgOriginal
@@ -108,8 +99,8 @@ def main():
         binary_mask = cv2.inRange(hsv_image, hsv_min, hsv_max)
         masked_image = draw_binary_mask(binary_mask, hsv_image)
 
-        cv2.imshow("ds",binary_mask)
-        low_threshold = 100
+
+        low_threshold = 50
         high_threshold = 150
         edges = cv2.Canny(masked_image, low_threshold, high_threshold)
 
@@ -119,14 +110,76 @@ def main():
         threshold = 15  # minimum number of votes (intersections in Hough grid cell)
         min_line_length = 20  # minimum number of pixels making up a line
         max_line_gap = 15  # maximum gap in pixels between connectable line segments
+        line_image = np.copy(newImage) * 0  # creating a blank to draw lines on
         lines = []
         # Run Hough on edge detected image
         # Output "lines" is an array containing endpoints of detected line segments
+        lines = cv2.HoughLinesP(edges, rho, theta, threshold, np.array([]),min_line_length, max_line_gap)
+
+        if lines is None:
+            print("Fuck, sorry my lord, but I did not found any line( But I am trying........")
+            continue
+
+        arrayLines = []
+        for line in lines:
+            for x1, y1, x2, y2 in line:
+                arrayLines.append([x1,y1,x2,y2])
 
 
+        # search the near lines from the center
+        nearLines = getTheNearestLine(arrayLines, cX1, cY1, line_image)
+
+        # draw that lines
+        cv2.line(line_image,
+                 (nearLines[0], nearLines[2]),
+                 (nearLines[1], nearLines[2]),
+                 (0, 255, 0), 1)
+
+        # center
+        cv2.line(line_image, (cX1, cY1-10), (cX1, cY1+10), (0, 255, 0), 2)
+        cv2.line(line_image, (cX1 - 10, cY1), (cX1 + 10, cY1), (0, 255, 0), 2)
+
+        needToControl = cX1
+
+        # center control
+        centerControl = int((nearLines[1] - nearLines[0]) / 2) + nearLines[0]
+        if (i == 0):
+            Xe = centerControl
+        centerControl = [kalman(centerControl), nearLines[2]]
+
+        # for control
+        etalonValue = centerControl[0]
+
+        cv2.line(line_image,
+                 (centerControl[0], centerControl[1] - 10),
+                 (centerControl[0],centerControl[1] + 10),
+                 (255, 255, 255), 2)
+        cv2.line(line_image,
+                 (centerControl[0] - 10, centerControl[1]),
+                 (centerControl[0] + 10, centerControl[1]),
+                 (255, 255, 255), 2)
+
+        #pprint("Need to control - " + str(needToControl))
+        #pprint("Etalon value - " + str(etalonValue))
+
+        # send to arduino
+        transferToArduino(etalonValue)
+        print(etalonValue)
+        cv2.line(newImage, (cX1, cY1 - 10), (cX1, cY1 + 10), (0, 255, 0), 2)
+        cv2.line(newImage, (cX1 - 10, cY1), (cX1 + 10, cY1), (0, 255, 0), 2)
+
+        for item in arrayLines:
+            cv2.line(line_image, (item[0], item[1]), (item[2], item[3]), (0, 0, 255), 1)
+
+        lines_edges = cv2.addWeighted(newImage, 0.8, line_image, 1, 0)
 
 
-        print(random.randint(0,100))
+        #cv2.imshow("binar mask", binary_mask)
+
+        #cv2.imshow("original", lines_edges)
+
+        #rawCapture.truncate()
+        #rawCapture.seek(0)
 
         i = 1
         if cv2.waitKey(1) & 0xFF == ord('q'):
